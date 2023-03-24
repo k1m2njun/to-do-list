9b3f52927cf223e7d4d8e595646b66d886f2fbcc
k1m2njun <k1m2njun@naver.com>
2023-03-24 21:33:21 +0900

    add repository

--- todo-server/src/main/java/org/example/TodoServerApplication.java
+++ todo-server/src/main/java/org/example/TodoServerApplication.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import org.example.repository.TodoRepository;
+
 public class TodoServerApplication {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
     }
 }
