5924bada448d457b048ce145c1575f6ca9aa1be4
k1m2njun <k1m2njun@naver.com>
2023-03-24 21:02:06 +0900

    1st Run

--- todo-server/src/main/java/org/example/TodoServerApplication.java
+++ todo-server/src/main/java/org/example/TodoServerApplication.java
@@ -1,7 +1,8 @@
 package org.example;
 
-public class Main {
+public class TodoServerApplication {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
